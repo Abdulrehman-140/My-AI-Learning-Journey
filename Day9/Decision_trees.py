3ede185e976162b48e95e49cdfab867b4f1e5e46
AbdulRehman <abdulrehman512671@gmail.com>
2026-01-07 12:56:49 +0500

    Add files via upload

---
+++ Day9/Decision_trees.py
@@ -0,0 +1,27 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+X = np.random.randn(100, 1)
+y = (X[:,0] > 0).astype(int)
+
+#Adding noise
+noise = np.random.randint(0, 2, size=100)
+y = np.logical_xor(y, noise).astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+model = DecisionTreeClassifier(max_depth=20)
+
+model.fit(X_train, y_train)
+
+print("Train Accuracy:", accuracy_score(y_train, model.predict(X_train)))
+print("Test Accuracy:", accuracy_score(y_test, model.predict(X_test)))
+
+
+#Challenge 1: The data is too clean to demonstrate overfitting and underfitting. Train and Test accuracy both are 1.0
+#Challenge 2: No difference after making this. and bro, You can't just give Decision trees and expect me to have a dream to understand it. And no proper challenges either
